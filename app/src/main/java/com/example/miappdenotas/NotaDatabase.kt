fc6476b37dfa484ec8ebd0aec6b45444050dc2b6
Gerardo Facundo <yerfac98@gmail.com>
2026-06-09 15:37:47 -0600

    Limpia configuracion de base de datos

--- app/src/main/java/com/example/miappdenotas/NotaDatabase.kt
+++ app/src/main/java/com/example/miappdenotas/NotaDatabase.kt
@@ -7,15 +7,15 @@ import androidx.room.RoomDatabase
 import com.example.miappdenotas.model.Nota
 import com.example.miappdenotas.model.NotaDao
 
-// @Database indica que esta es la clase de la base de datos
-// Ya no necesitamos @TypeConverters, ya que Nota.fecha es un Long
-@Database(entities = [Nota::class], version = 1, exportSchema = false)
+@Database(
+    entities = [Nota::class],
+    version = 1,
+    exportSchema = false
+)
 abstract class NotaDatabase : RoomDatabase() {
 
-    // Función abstracta para obtener el DAO
     abstract fun obtenerNotaDao(): NotaDao
 
-    // Companion Object para implementar el patrón Singleton
     companion object {
         @Volatile
         private var INSTANCIA: NotaDatabase? = null
@@ -25,12 +25,8 @@ abstract class NotaDatabase : RoomDatabase() {
                 val instancia = Room.databaseBuilder(
                     context.applicationContext,
                     NotaDatabase::class.java,
-                    "nota_db" // Nombre del archivo de la base de datos
-                )
-                    // CORRECCIÓN: Se elimina .allowMainThreadQueries()
-                    // Las operaciones de Room ahora DEBEN ejecutarse con 'suspend'
-                    // o dentro de un 'CoroutineScope' para evitar bloqueos de UI.
-                    .build() // Crea e inicializa la base de datos
+                    "nota_db"
+                ).build()
 
                 INSTANCIA = instancia
                 instancia
